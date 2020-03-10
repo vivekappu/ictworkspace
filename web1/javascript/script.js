45e5559f5df3cb0a8d14730f4106976198c76654
vivek <vivek.appu619@gmail.com>
2020-03-10 12:45:52 +0530

    uppercase and length

--- web1/javascript/script.js
+++ web1/javascript/script.js
@@ -5,3 +5,9 @@ name="mohanlal";
 age=55;
 console.log("my name is "+name+" and iam "+age+"year's old");//concatenation
 console.log(`my name is ${name} and iam ${age} year's old`);//template method
+const details=`my name is ${name} and iam ${age} year's old`;//assignment method
+console.log(details);
+console.log(details.length);// to find length of string , string is an object in js
+console.log(details.toUpperCase());//to uppercase method
+console.log(details.substring(0,4));
+console.log(details.substring(0,5).toUpperCase());// Nitha mam number 9645612250 , 9349936566
